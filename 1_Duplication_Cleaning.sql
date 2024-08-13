dacc6a3d165cbc05eec2f47b77124e643ef4eb96
Adeel-Shah96 <adeelahmedshah@hotmail.co.uk>
2024-08-13 18:55:45 +0100

    Create 1_Duplication_Cleaning.sql
    Query for removing duplicate records in BigQuery Tables

---
+++ 1_Duplication_Cleaning.sql
@@ -0,0 +1,23 @@
+Data cleaning
+==============
+
+-- In order to remove duplications from tables in BigQuery
+
+-- For Installs table
+
+CREATE OR REPLACE TABLE `datataskjp.marketing.Installs`
+AS
+SELECT DISTINCT * FROM `datataskjp.marketing.Installs`
+
+-- For Ad Spend table  
+CREATE OR REPLACE TABLE `datataskjp.marketing.ad_spend_installs`
+AS
+SELECT DISTINCT * FROM `datataskjp.marketing.ad_spend_installs`
+
+-- For revenue table  
+CREATE OR REPLACE TABLE `datataskjp.marketing.revenue`
+AS
+SELECT DISTINCT * FROM `datataskjp.marketing.revenue`
+
+-- Checking data
+SELECT * FROM `datataskjp.marketing.revenue`
