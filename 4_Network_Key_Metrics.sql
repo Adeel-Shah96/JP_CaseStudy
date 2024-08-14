0fa8a3369e5b8362a88b5289dbef61c97fa39498
Adeel-Shah96 <adeelahmedshah@hotmail.co.uk>
2024-08-14 21:29:15 +0100

    Create 4_Network_Key_Metrics.sql

---
+++ 4_Network_Key_Metrics.sql
@@ -0,0 +1,36 @@
+-- Start by calculating DAU and eDAU in individual CTEs
+
+WITH edau_platform AS (
+  SELECT
+  platform,
+  count(distinct(userId)) as eDAU
+  from `datataskjp.marketing.revenue_cleaned`
+  where platform is not null
+  group by 1
+),
+
+dau_platform AS (
+  SELECT
+  os_name as platform,
+  count(distinct(userId)) as DAU
+  from `datataskjp.marketing.Installs`
+  where os_name is not null
+  group by 1
+)
+
+SELECT
+  network,
+  rc.platform,
+  edp.eDAU,
+  dp.DAU,
+  sum(amount) as total_revenue,
+  count(*) as impressions,
+  1000*(SUM(amount)/count(*)) as eCPM,
+  sum(amount) / dp.DAU as ARPDAU,
+  sum(amount) / edp.eDAU as ARPeDAU,
+  count(*) / dp.DAU as Imps_per_DAU,
+  count(*) / edp.eDAU as Imps_per_eDAU
+FROM `datataskjp.marketing.revenue_cleaned` rc join edau_platform edp on rc.platform = edp.platform
+join dau_platform dp on rc.platform = dp.platform
+group by 1,2,3,4
+order by network
